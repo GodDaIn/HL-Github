14df798e191e26f1d269d6cfbbd45a73f35632c2
GodDaIn <ledain5094@gmail.com>
2020-06-03 20:26:41 +0900

    두번째 커밋입니다.

--- github.c
+++ github.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World");
+int main(){
+    int x, y, type;
+    int result = 0;
+    
+    printf("Ã¹¹øÂ° ¼ýÀÚ¸¦ ÀÔ·ÂÇÏ¼¼¿ä. : ");
+    scanf("%d", &x);
+    
+    printf("µÎ¹øÂ° ¼ýÀÚ¸¦ ÀÔ·ÂÇÏ¼¼¿ä. : ");
+    scanf("%d", &y);
+    
+    printf("µ¡¼À(0)°ö¼À(1)³ª´°¼À(2)»¬¼À(3) : ");
+    scanf("%d", &type);
+    
+    switch(type){
+        case 0:
+            result = x + y;
+        break;
+        
+        case 1:
+            result = x * y;
+        break;
+        
+        case 2:
+            result = x / y;
+        break;
+        
+        case 3:
+            result = x - y;
+        break;
+    }
+    
+    printf("result : %d", result);
 
     return 0;
 }
