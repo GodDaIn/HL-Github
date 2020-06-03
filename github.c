770c98a24fd22e0a1cad3b5ebf88403d9f90ac4e
GodDaIn <ledain5094@gmail.com>
2020-06-03 20:28:04 +0900

    세번째 커밋입니다.

--- github.c
+++ github.c
@@ -10,26 +10,7 @@ int main(){
     printf("µÎ¹øÂ° ¼ýÀÚ¸¦ ÀÔ·ÂÇÏ¼¼¿ä. : ");
     scanf("%d", &y);
     
-    printf("µ¡¼À(0)°ö¼À(1)³ª´°¼À(2)»¬¼À(3) : ");
-    scanf("%d", &type);
-    
-    switch(type){
-        case 0:
-            result = x + y;
-        break;
-        
-        case 1:
-            result = x * y;
-        break;
-        
-        case 2:
-            result = x / y;
-        break;
-        
-        case 3:
-            result = x - y;
-        break;
-    }
+    result = x % y;
     
     printf("result : %d", result);
 
